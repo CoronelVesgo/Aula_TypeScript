bceee6e5d61ad2ba6a9d34c3f59d51b588b3a990
CoronelVesgo <l.makoto.oliveira@gmail.com>
2024-09-06 10:13:09 -0300

    Update

--- exercicio1_1.ts
+++ exercicio1_1.ts
@@ -6,17 +6,10 @@ interface Produto {
 
 type FormaPagamento = 'dinheiro' | 'cartão' | 'pix';
 
-function Transacao(produto: Produto, pagamento: FormaPagamento): string {
-    return `Produto: ${produto.nome}, Categoria: ${produto.categoria}, Preço:
-    R$${produto.preco.toFixed(2)}, Forma de Pagamento: ${pagamento}.`;
-};
-
-const produto: Produto = {
-    nome: 'Dark Sus',
-    preco: 399,
-    categoria: 'Jogo',
+function exibirTransacao(produto: Produto, pagamento: FormaPagamento): string {
+    return 'produto: ${produto.nome}, Categoria: ${produto.categoria}, Preco: R$${produto.preco.toFixed(2)}, Forma de Pagamento: ${pagamento}.';
 }
+const expTransacao: Produto = { nome: 'PC', preco: 5000, categoria: 'eletronico' } 
+const expFormaPagamento: FormaPagamento = 'dinheiro';
 
-const formaPagamento: FormaPagamento = 'dinheiro';
-
-console.log(produto, formaPagamento)
+console.log(exibirTransacao(expTransacao, expFormaPagamento));
